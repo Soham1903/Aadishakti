13356b93ba684ecc612b00babf2fd2b1fcd177f4
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-03-13 12:56:50 +0530

    Course api added

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -10,6 +10,7 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
 });
 const upload = multer({ storage });
+const temp = 1;
 
 // Course routes
 router.post("/add", upload.single("image"), addCourse);
--- frontend/aadishakti/src/App.jsx
+++ frontend/aadishakti/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from './components/Home/Home';
+import Home from "./components/Home/Home";
 // import Carousel from "./components/Home/imageCarousel";  // Import Carousel
 import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
@@ -9,7 +9,8 @@ import ResetPassword from "./components/ResetPassword";
 import UploadImage from "./components/Uploadimg";
 import CourseForm from "./components/CourseForm";
 import Courses from "./components/Courses";
-import Footer from "./components/Footer";  
+import CoursesSection from "./components/CourseSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -17,14 +18,13 @@ function App() {
       <Navbar />
       <Routes>
         {/* <Route path="/" element={<Carousel />} />  Set Carousel as Home Page */}
-        <Route path="/" element={<Home />} />  Set Carousel as Home Page
-
+        <Route path="/" element={<Home />} /> Set Carousel as Home Page
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/upload" element={<UploadImage />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses" element={<CoursesSection />} />
         <Route path="/course-form" element={<CourseForm />} />
       </Routes>
       <Footer />
