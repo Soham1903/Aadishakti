935096e01a45a8d7a1be5f2a744422c9f7c4f7ef
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-03-23 23:23:45 +0530

    promocode add form

--- frontend/aadishakti/src/App.jsx
+++ frontend/aadishakti/src/App.jsx
@@ -17,39 +17,38 @@ import Footer from "./components/Footer";
 import CourseDetails from "./components/CourseDetails";
 import { UserProvider } from "./UserContext.jsx";
 import { CartProvider } from "./contexts/CartContext";
-import Dashboard from './components/Dashboard';
+import Dashboard from "./components/Dashboard";
 import Cart from "./components/Cart.jsx";
+import PromoCodeForm from "./components/PromoCodeForm.jsx";
 
 function App() {
   return (
     <UserProvider>
       <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          {/* Set Carousel as Home Page */}
-          {/* <Route path="/" element={<Carousel />} /> */}
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/upload" element={<UploadImage />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/courses/:title" element={<CourseDetails />} />
-          <Route path="/course-form" element={<CourseForm />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/upload-course" element={<UploadCourse />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          
-        </Routes>
-        <Footer />
-      </Router>
-
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* Set Carousel as Home Page */}
+            {/* <Route path="/" element={<Carousel />} /> */}
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/upload" element={<UploadImage />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/courses/:title" element={<CourseDetails />} />
+            <Route path="/course-form" element={<CourseForm />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/upload-course" element={<UploadCourse />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/promocode-form" element={<PromoCodeForm />} />
+          </Routes>
+          <Footer />
+        </Router>
       </CartProvider>
-      
     </UserProvider>
   );
 }
