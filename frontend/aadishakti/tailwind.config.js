60dc34f38ecf0627f982daa90ce3d5cb58b0b55c
aditi gupta <aditigupta72003@gmail.com>
2025-05-12 11:28:38 +0530

    font

--- frontend/aadishakti/tailwind.config.js
+++ frontend/aadishakti/tailwind.config.js
@@ -1,17 +1,33 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         maroon: {
+//           100: '#C75B7A',
+//           200: '#921A40',
+//           300: '#D9ABAB',
+//           400: '#F4D9D0',
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        maroon: {
-          100: '#C75B7A',
-          200: '#921A40',
-          300: '#D9ABAB',
-          400: '#F4D9D0',
-        },
+      fontFamily: {
+        sans: ['Poppins'],
       },
     },
   },
-  plugins: [],
+plugins: [],
 };
